e669830b841ee8ec100ebfb47fe539ed3ff1c937
Pavel Viznitsa <viznitsap@gmail.com>
2022-04-07 14:50:51 +0300

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,5 @@
-
-    public static void main(String[] args) {
-        System.out.print("Hello Java");
-    }
-
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello Java");
+  }
+}
